226a2148150208f6c06acdfd2c0fd5396b5fbc83
Camden Miller <cammiller42003@gmail.com>
2024-11-14 14:03:04 -0700

    help->faq

--- airprosolutions/src/Pages/Homeowners.js
+++ airprosolutions/src/Pages/Homeowners.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function HomeownerPage({ title, children }) {
   const navigate = useNavigate();
+  const elfsightRef = useRef(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://static.elfsight.com/platform/platform.js';
+    script.async = true;
+    script.onload = () => {
+      // Ensure Elfsight is initialized
+      if (window.ElfsightPlatform) {
+        window.ElfsightPlatform.init();
+      }
+    };
+    elfsightRef.current.appendChild(script);
+  }, []);
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
@@ -12,7 +26,6 @@ function HomeownerPage({ title, children }) {
   };
 
   const navigateToServicesPage = () => {
-    // Pass the selected category and filter data as part of the state
     navigate(`/services/Homeowners`, {
       state: {
         selectedCategory: 'Homeowners',
@@ -24,23 +37,26 @@ function HomeownerPage({ title, children }) {
     });
   };
 
-
   return (
     <div style={{ fontFamily: 'Poppins, sans-serif', padding: '20px' }}>
       <h1 style={{ textAlign: 'center', fontSize: '3rem', color: '#333' }}>{title}</h1>
       <div style={{ marginTop: '20px' }}>
-      <h1>Homeowners</h1>
+        <h1>Homeowners</h1>
+        <p>As licensed HVAC professionals in Southern Utah, we deliver quality, safety, and expertise in every project, meeting all state certifications and tailored to the unique needs of our local climate.</p>
 
-      <div style={{ marginTop: '40px' }}>
-        <button 
-          onClick={navigateToServicesPage}
-          style={{ backgroundColor: '#28a745', color: 'white', padding: '10px 20px', border: 'none', cursor: 'pointer' }}
-        >
-          View Services for Homeowners
-        </button>
-      </div>
+        <div style={{ marginTop: '40px' }}>
+          <button 
+            onClick={navigateToServicesPage}
+            style={{ backgroundColor: '#28a745', color: 'white', padding: '10px 20px', border: 'none', cursor: 'pointer' }}
+          >
+            View Services for Homeowners
+          </button>
 
-
+          {/* Elfsight widget container */}
+          <div ref={elfsightRef} style={{ marginTop: '20px' }}>
+            <div className="elfsight-app-1a8f76dc-c788-4556-a759-186486a76b25"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
